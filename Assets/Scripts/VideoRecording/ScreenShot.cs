28c74b654ee1d3968f5d53b56b85804c6b8bc286
Corey Novich <conovich@gmail.com>
2015-07-13 23:25:48 -0400

    Screenshots save to .png files!

---
+++ Assets/Scripts/VideoRecording/ScreenShot.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenShot : MonoBehaviour {
+
+	bool takeHiResShot = false;
+
+	public static string ScreenShotName(int width, int height) {
+		return string.Format("{0}/screenshots/screen_{1}x{2}_{3}.png", 
+		                     Application.dataPath, 
+		                     width, height, 
+		                     System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
+	}
+
+	void LateUpdate() {
+		takeHiResShot = Input.GetKeyDown("k");
+		if (takeHiResShot) {
+			StartCoroutine(TakeScreenshot());
+		}
+	}
+
+	IEnumerator TakeScreenshot(){
+		// We should only read the screen buffer after rendering is complete
+		yield return new WaitForEndOfFrame();
+		
+		// Create a texture the size of the screen, RGB24 format
+		int width = Screen.width;
+		int height = Screen.height;
+		Texture2D tex = new Texture2D(width, height, TextureFormat.RGB24, false);
+		
+		// Read screen contents into the texture
+		tex.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+		tex.Apply();
+		
+		// Encode texture into PNG
+		byte[] bytes = tex.EncodeToPNG();
+		Destroy(tex);
+		
+		// For testing purposes, also write to a file in the project folder
+		string filename = ScreenShotName(width, height);
+		System.IO.File.WriteAllBytes(filename, bytes);
+
+		/*
+		yield return new WaitForEndOfFrame ();
+
+		RenderTexture rt = new RenderTexture(resWidth, resHeight, 24);
+		GetComponent<Camera>().targetTexture = rt;
+		Texture2D screenShot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
+		GetComponent<Camera>().Render();
+		RenderTexture.active = rt;
+		screenShot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
+		GetComponent<Camera>().targetTexture = null;
+		RenderTexture.active = null; // JC: added to avoid errors
+		Destroy(rt);
+		byte[] bytes = screenShot.EncodeToPNG();
+		string filename = ScreenShotName(resWidth, resHeight);
+		System.IO.File.WriteAllBytes(filename, bytes);
+		Debug.Log(string.Format("Took screenshot to: {0}", filename));
+		takeHiResShot = false;
+*/
+	}
+
+}
